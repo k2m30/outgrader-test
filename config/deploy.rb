fd2cd9bc01cc42bdb43f297203b4458b5b92ae53
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-01-09 21:01:05 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,10 +48,10 @@ namespace :deploy do
 
   desc 'Make sure local git is in sync with remote.'
   task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse outgrader-test/master`
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts 'WARNING: HEAD is not the same as outgrader-test/master'
       puts 'Run `git push` to sync changes.'
-      # exit #
+      exit
     end
   end
   before 'deploy', 'deploy:check_revision'
