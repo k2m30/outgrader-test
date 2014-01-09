14c036d562eecb88f32fb2752e6a7650ce395125
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-01-09 17:34:18 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,6 @@ namespace :deploy do
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
       run "/etc/init.d/unicorn_#{application} #{command}"
-      run "RAILS_ENV=production #{current_path}/bin/delayed_job #{command}"
     end
   end
 
